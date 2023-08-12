959653aa8fa0072fbf0272aebc503eb34ac4af36
felixchua3143 <felixchua3142@gmail.com>
2023-08-12 13:02:58 +0800

    s

--- javascript files/background-random.js
+++ javascript files/background-random.js
@@ -6,8 +6,6 @@ function changeBg() {
         'url("./images/144.webp")',
         'url("./images/148.webp")',
         'url("./images/152.webp")',
-        'url("./images/IMG_5582.webp")',
-        'url("./images/IMG_5646.webp")',
         'url("./images/IMG_5376.webp")',
         'url("./images/IMG_5842.webp")',
         'url("./images/IMG_8501.webp")',
